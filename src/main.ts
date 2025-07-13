244790a50ac52916a0264bf53b48f817a09bbf11
NAT1804 <tuanna184.dev@gmail.com>
2025-07-13 22:02:52 +0700

    feature: add scene

--- src/main.ts
+++ src/main.ts
@@ -4,7 +4,18 @@ import { OrbitControls } from "three/addons/controls/OrbitControls.js";
 import Stats from "three/addons/libs/stats.module.js";
 import { GUI } from "dat.gui";
 
-const scene = new THREE.Scene();
+const sceneA = new THREE.Scene();
+sceneA.background = new THREE.Color(0x123456);
+
+const sceneB = new THREE.Scene();
+sceneB.background = new THREE.TextureLoader().load(
+  "https://sbcode.net/img/grid.png"
+);
+
+const sceneC = new THREE.Scene();
+sceneC.background = new THREE.CubeTextureLoader()
+  .setPath("https://sbcode.net/img/")
+  .load(["px.png", "nx.png", "py.png", "ny.png", "pz.png", "nz.png"]);
 
 const camera = new THREE.PerspectiveCamera(
   75,
@@ -30,12 +41,29 @@ const geometry = new THREE.BoxGeometry();
 const material = new THREE.MeshNormalMaterial({ wireframe: true });
 
 const cube = new THREE.Mesh(geometry, material);
-scene.add(cube);
+sceneA.add(cube);
 
 const stats = new Stats();
 document.body.appendChild(stats.dom);
 
+let activeScene = sceneA;
+const setScene = {
+  sceneA: () => {
+    activeScene = sceneA;
+  },
+  sceneB: () => {
+    activeScene = sceneB;
+  },
+  sceneC: () => {
+    activeScene = sceneC;
+  },
+};
+
 const gui = new GUI();
+const sceneFolder = gui.addFolder("Scene");
+sceneFolder.add(setScene, "sceneA").name("Scene A");
+sceneFolder.add(setScene, "sceneB").name("Scene B");
+sceneFolder.add(setScene, "sceneC").name("Scene C");
 
 const cubeFolder = gui.addFolder("Cube");
 cubeFolder.add(cube.rotation, "x", 0, Math.PI * 2);
@@ -50,14 +78,10 @@ cameraFolder.open();
 function animate() {
   requestAnimationFrame(animate);
 
-  // stats.begin();
-
   // cube.rotation.x += 0.01;
   // cube.rotation.y += 0.01;
 
-  // stats.end();
-
-  renderer.render(scene, camera);
+  renderer.render(activeScene, camera);
 }
 
 animate();
